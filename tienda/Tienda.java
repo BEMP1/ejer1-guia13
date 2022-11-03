ebb9ddebde0b7f1fb78627504a8dc2b7d78b75ab
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-11-02 21:37:04 -0300

    Add files via upload

---
+++ tienda/Tienda.java
@@ -0,0 +1,14 @@
+
+package tienda;
+
+import tienda.persistencia.DAO;
+
+
+public class Tienda {
+
+
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ tienda/entidades/Fabricante.java
@@ -0,0 +1,27 @@
+package tienda.entidades;
+
+public class Fabricante {
+
+    private int codigo;
+    private String nombre;
+
+    public Fabricante() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
